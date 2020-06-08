5c6079ee36a8ca0ab7529e3c223c6025fabc859a
Brandon Zhu <bzhu@utraque.com>
2020-06-08 00:59:01 -0400

    132. More Automatic Context Creation

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,26 +1,22 @@
-import React, { useReducer } from 'react';
+import createDataContext from './createDataContext';
 
-const BlogContext = React.createContext();
-
-const blogReducer = (state, action)=> {
-  switch(action.type) {
+const blogReducer = (state, action) => {
+  switch (action.type) {
     case 'add_blogpost':
-      return [...state, {title: `BlogPost #${state.length+1}`}];
+      return [...state, { title: `BlogPost #${state.length + 1}` }];
     default:
       return state;
   }
-}
-
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, dispatch] = useReducer(blogReducer, []);
-
-  const addBlogPost = () => {
-    dispatch({type: 'add_blogpost'})
-  }
-
-  return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }} >
-    {children}
-  </BlogContext.Provider >;
 };
 
-export default BlogContext;
+const addBlogPost = (dispatch) => {
+  return () => {
+    dispatch({ type: 'add_blogpost' });
+  }
+};
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
+);
