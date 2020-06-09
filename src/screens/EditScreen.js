11003936269714827b1fed274f1904b90c40c5e2
Brandon Zhu <bzhu@utraque.com>
2020-06-09 01:10:05 -0400

    146. Extracting Form Logic

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,25 +1,16 @@
-import React, { useContext, useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
 
 import { Context } from '../context/BlogContext';
-import { TextInput } from 'react-native-gesture-handler';
+import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({ navigation }) => {
   const { state } = useContext(Context);
 
   const blogPost = state.find(
-    blogPost => blogPost.id === navigation.getParam('id')
-  );
+    blogPost => blogPost.id === navigation.getParam('id'));
 
-  const [title, setTitle] = useState(blogPost.title);
-  const [content, setContent] = useState(blogPost.content);
-
-  return <View>
-    <Text>Edit Title:</Text>
-    <TextInput value={title} onChangeText={setTitle}/>
-    <Text>Edit Content:</Text>
-    <TextInput value={content} onChangeText={setContent}/>
-  </View>
+  return <BlogPostForm />
 };
 
 const styles = StyleSheet.create({});
