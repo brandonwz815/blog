a1dee8a07f8cbccec65db55297d4d1103657e3eb
Brandon Zhu <bzhu@utraque.com>
2020-06-09 20:40:50 -0400

    154 Navigating Backwards

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -5,7 +5,7 @@ import { Context } from '../context/BlogContext';
 import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({ navigation }) => {
-  const id  = navigation.getParam('id');
+  const id = navigation.getParam('id');
   const { state, editBlogPost } = useContext(Context);
 
   const blogPost = state.find(
@@ -14,7 +14,7 @@ const EditScreen = ({ navigation }) => {
   return <BlogPostForm
     initialValues={{ title: blogPost.title, content: blogPost.content }}
     onSubmit={(title, content) => {
-      editBlogPost(id, title, content);
+      editBlogPost(id, title, content, () => navigation.pop());
     }} />
 };
 
